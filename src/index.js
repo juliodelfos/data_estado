1c264dd7ba3f765e1924a21545bbb47878b0fb6b
yob <yolivares@mintrab.gob.cl>
2024-12-21 20:18:06 -0300

    Se añade gitignore

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import ministeriosRouter from './routes/ministerios.js';
+import express from "express";
+import cors from "cors";
+import ministeriosRouter from "./routes/ministerios.js";
+import "dotenv/config"; // Carga las variables .env en process.env
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Montar /api
-app.use('/api', ministeriosRouter);
+app.use("/api", ministeriosRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
