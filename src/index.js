373e6b2cb002e3c1719acb429c369944bd8444f5
yob <yersonolivares@protonmail.com>
2024-12-21 21:15:30 -0300

    Actualiza index

--- src/index.js
+++ src/index.js
@@ -10,7 +10,4 @@ app.use(express.json());
 // Montar /api
 app.use("/api", ministeriosRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
-});
+export default app;
